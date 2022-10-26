a01eac6bae09b43ab1fe90f34219d36dc6d176c2
Gabriela Meirelles <gb.meirellesmartins@gmail.com>
2022-10-26 09:07:46 +0100

    "lab finished"

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.get('/home', (req, res, next) => {
+  res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/about', (req, res, next) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/gallery', (req, res, next) => {
+    res.sendFile(__dirname + '/views/gallery.html');
+  });
+
+app.get('/works', (req, res, next) => {
+    res.sendFile(__dirname + '/views/works.html');
+  });
+
+app.listen(3000, () => console.log('App running on port 3000'));
